160a1df7bbd5e8d44e4c49293b1dedc2b2208507
brunoorlandelli <brunosorlandelli@gmai.com>
2019-12-17 21:25:14 -0300

    Add ChaseBall and MarkEnemy classes

---
+++ Sample Teams/src/team/project/rioters/ChaseBall.java
@@ -0,0 +1,19 @@
+package team.project.rioters;
+
+import behavior_tree.BTNode;
+import behavior_tree.BTStatus;
+import easy_soccer_lib.utils.Vector2D;
+
+public class ChaseBall extends BTNode<CommandPlayer> {
+
+	@Override
+	public BTStatus tick(CommandPlayer agent) {
+		if (agent.isPointsAreClose(agent.getPosition(), agent.getBallPos(), 1.0d)) {
+			return BTStatus.SUCCESS;
+		} else {
+			agent.dash(agent.getBallPos(), 100);
+			return BTStatus.RUNNING;
+		}
+	}
+
+}
---
+++ Sample Teams/src/team/project/rioters/MarkEnemy.java
@@ -0,0 +1,22 @@
+package team.project.rioters;
+
+import behavior_tree.BTNode;
+import behavior_tree.BTStatus;
+
+public class MarkEnemy extends BTNode<CommandPlayer> {
+
+	@Override
+	public BTStatus tick(CommandPlayer agent) {
+		if(agent.isPointsAreClose(agent.getPosition(), agent.getBallPos(), 15)) {
+			if(agent.isPointsAreClose(agent.getPosition(), agent.closestEnPlayer.getPosition(), 2)) {
+				return BTStatus.SUCCESS;
+			}else {
+				dash(agent.closestEnPlayer.getPosition(), 100);
+				return BTStatus.RUNNING;
+			}
+		}else {
+			return BTStatus.FAILURE;
+		}
+	}
+
+}
